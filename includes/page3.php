39bc9b0faf09bbcd4006aa25cda6e4afe850a3bc
OuterAlchemy <contact.thomasmerlin@gmail.com>
2016-07-23 17:56:17 +0200

    Ajout dossier includes / Ajout fichier CSS / Début de la page 3

---
+++ includes/page3.php
@@ -0,0 +1,25 @@
+<section class="l-accordion" id="section3">
+	<div class="l-accordion__slide" data-accordion-slide="1">
+		<div class="l-accordion__inside">
+			<h1>Maçonnerie</h1>
+		</div>
+	</div>
+	
+	<div class="l-accordion__slide" data-accordion-slide="2">
+		<div class="l-accordion__inside">
+			<h1>T.P</h1>
+		</div>
+	</div>
+	
+	<div class="l-accordion__slide" data-accordion-slide="3">
+		<div class="l-accordion__inside">
+			<h1>Carrelage</h1>
+		</div>
+	</div>
+	
+	<div class="l-accordion__slide" data-accordion-slide="4">
+		<div class="l-accordion__inside">
+			<h1>Menuiserie</h1>
+		</div>
+	</div>
+</section>
