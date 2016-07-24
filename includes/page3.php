afcffe38d0f688fbb65acd64b4b357ced653d8b2
OuterAlchemy <contact.thomasmerlin@gmail.com>
2016-07-24 13:37:55 +0200

    Modifications page3

--- includes/page3.php
+++ includes/page3.php
@@ -1,25 +1,25 @@
 <section class="l-accordion" id="section3">
 	<div class="l-accordion__slide" data-accordion-slide="1">
 		<div class="l-accordion__inside">
-			<h1>Maçonnerie</h1>
+			<h2>Maçonnerie</h2>
 		</div>
 	</div>
 	
 	<div class="l-accordion__slide" data-accordion-slide="2">
 		<div class="l-accordion__inside">
-			<h1>T.P</h1>
+			<h2>T.P</h2>
 		</div>
 	</div>
 	
 	<div class="l-accordion__slide" data-accordion-slide="3">
 		<div class="l-accordion__inside">
-			<h1>Carrelage</h1>
+			<h2>Carrelage</h2>
 		</div>
 	</div>
 	
 	<div class="l-accordion__slide" data-accordion-slide="4">
 		<div class="l-accordion__inside">
-			<h1>Menuiserie</h1>
+			<h2>Menuiserie</h2>
 		</div>
 	</div>
 </section>
