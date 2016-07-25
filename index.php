762dd37ee7d8bfb9bff078eba6631de940533ec0
Emilie Chartier <emilie.chartier@sfr.fr>
2016-07-25 18:02:20 +0200

    ajout contact

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   
     <meta name="description" content="MCTP site de maçonnerie">
     <meta name="keywords" content="maçonnerie, matériaux, maçon, béton">
-    <meta property="og:url" content="http://codepen.io/hrtzt/details/NPZKRN">
+    
     <meta property="og:image" content="https://pbs.twimg.com/media/CCNJN_XUMAAJSzU.jpg:large">
     
     <link rel="stylesheet" href="css/normalize.css">
@@ -15,6 +15,7 @@
     <link rel="stylesheet" type="text/css" href="./css/accordeonP3.css" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="js/prefixfree.min.js"></script>
+    
 
     
   </head>
@@ -39,8 +40,6 @@
           <div class="page" id="p2">
             <section class="icon fa fa-users"><span class="title">Qui sommes-nous ?</span>
           
-
-
             </section>
           </div>  
           <div class="page" id="p3">
@@ -57,31 +56,54 @@
           </div> 
           <div class="page" id="p5">
             <section class="icon fa fa-plus-envelope">
-              <span class="title">Contactez-nous</span>
-                <div id="form-main">
-  <div id="form-div">
-    <form class="form" id="form1" method="post" action="traitement.php">
-      
-      <p class="name">
-        <input name="name" type="text" class="validate[required,custom[onlyLetter],length[0,100]] feedback-input" placeholder="Nom" id="name" />
-      </p>
-      
-      <p class="email">
-        <input name="email" type="text" class="validate[required,custom[email]] feedback-input" id="email" placeholder="Email" />
-      </p>
-      
-      <p class="text">
-        <textarea name="text" class="validate[required,length[6,300]] feedback-input" id="comment" placeholder="Commentaire"></textarea>
-      </p>
-      
-      
-      <div class="submit">
-        <input type="submit" value="Envoyer" id="button-blue"/>
-        <div class="ease"></div>
-      </div>
-    </form>
-  </div>
               
+                <div id="form-main">
+  <div id="container"></div>
+  <script id="template">
+    <div class="flip-card" on-click="toggle('flipCard')">{{ flipCard ? 'Renvoyer un mail' : 'Contactez-nous' }}</div>
+    <div class="contact-wrapper">
+        <div class="envelope {{ flipCard ? 'active' : '' }}">
+          <div class="back paper"></div>
+          <div class="content">
+            <div class="form-wrapper">
+              <form>
+                <div class="top-wrapper">
+                  <div class="input">
+                    <label>Nom</label>
+                    <input type="text" name="name"/>
+                  </div>
+                  <div class="input">
+                    <label>Téléphone</label>
+                    <input type="text" name="phone"/>
+                  </div>
+                  <div class="input">
+                    <label>Email</label>
+                    <input type="text" name="_replyto"/>
+                  </div>
+                </div>
+                <div class="bottom-wrapper">
+                  <div class="input">
+                    <label>Type de travaux</label>
+                    <input type="text" name="_subject"/>
+                  </div>
+                  <div class="input">
+                    <label>Message</label>
+                    <textarea rows="5" name="message"></textarea>
+                  </div>
+                  <div class="submit">
+                    <div class="submit-card" on-click="toggle('flipCard')">Envoyer</div>
+                  </div>
+                </div>
+              </form>
+              
+            </div>
+
+          </div>
+
+          <div class="front paper"></div>
+        </div>
+      </div>
+              </script>
             </section>
           </div> 
         </div>
@@ -90,16 +112,18 @@
   </div>
 </div>
 <footer>
-  <div class="footer-copyright grey darken-3">
+  <div class="footer">
             <div class="container">
                 © 2016 Tous droits réservés
             </div>
         </div>
 </footer>
     
+
+        <script src='http://cdn.ractivejs.org/latest/ractive.js'></script>
         <script src="js/index.js"></script>
         <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
+        
 
     
     
