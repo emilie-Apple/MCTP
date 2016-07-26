c929b20c112ba6970fc8c72de96558a2b11e027c
OuterAlchemy <contact.thomasmerlin@gmail.com>
2016-07-26 09:20:03 +0200

    MOdifications page 4

--- includes/page3.php
+++ includes/page3.php
@@ -1,25 +1,28 @@
 <section class="l-accordion" id="section3">
 	<div class="l-accordion__slide" data-accordion-slide="1">
 		<div class="l-accordion__inside">
-			<h2>Maçonnerie</h2>
-		</div>
-	</div>
+			<h2>
+                Maçonnerie
+            </h2>          
+        </div>
+    </div>
+	
 	
 	<div class="l-accordion__slide" data-accordion-slide="2">
-		<div class="l-accordion__inside">
-			<h2>T.P</h2>
-		</div>
-	</div>
-	
-	<div class="l-accordion__slide" data-accordion-slide="3">
 		<div class="l-accordion__inside">
 			<h2>Carrelage</h2>
 		</div>
 	</div>
 	
+	<div class="l-accordion__slide" data-accordion-slide="3">
+		<div class="l-accordion__inside">
+			<h2 id="categorie3">Terrassement</h2>
+		</div>
+	</div>
+	
 	<div class="l-accordion__slide" data-accordion-slide="4">
 		<div class="l-accordion__inside">
-			<h2>Menuiserie</h2>
+			<h2>Placo</h2>
 		</div>
 	</div>
 </section>
---
+++ js/loading.js
@@ -0,0 +1,15 @@
+$(window).load(function() {
+
+  setTimeout(function() {
+    $('#CJ-Overlay').fadeOut(800);
+  }, 2500);
+
+  setTimeout(function() {
+    $('#loading, #loading2').fadeOut(400);
+  }, 1500);
+  setTimeout(function() {
+
+    $('.overlay').fadeOut(800);
+  }, 3000);
+
+});
