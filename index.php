a547b46da2948e4ef52b57bbde91da68c696c069
Emilie Chartier <emilie.chartier@sfr.fr>
2016-07-27 10:34:48 +0200

    ajout css menu

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" type="text/css" href="./css/accordeonP3.css" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="js/prefixfree.min.js"></script>
-    <script src="js/p2.min.js"></script>
+    
     
   </head>
 
@@ -39,10 +39,11 @@
           <section class="icon fa fa-users"><span class="title">Qui sommes-nous ?</span>
           <div id="circle-shape-example">
   <img src="css/img/logo.png" alt="logo" class="logo">
-  <p>L'entreprise M-C-T-P situé dans la région du Loiret à Nangeville (45330) <br/> est une entreprise de maçonnerie générale créée en 2013 par Fabien TECHER.</p>
-  <p>Avec l’aide de son associé Grégory et d’une équipe de 4 personnes, l’entreprise <br/> M-C-T-P met toutes ses compétences et son savoir faire à votre service .<br/>
-  Des petits travaux de maçonnerie, réparations, rénovations complètes, constructions de maisons neuves, nous réalisons tous vos travaux de maçonnerie et de gros œuvre.</p>
-  <p>Vous avez un projet de construction, d'agrandissement ou de rénovation,<br/> nous sommes à votre écoute pour vous apporter la réponse la mieux adaptée et vous accompagner dans sa réalisation.
+  <h4>Présentation</h4>
+  <p>L'entreprise <strong>M-C-T-P </strong> situé dans la région du Loiret à Nangeville (45330) <br/> est une entreprise de maçonnerie générale créée en 2013 par Fabien TECHER.</p>
+  <p>Avec l’aide de son associé Grégory et d’une équipe de 4 personnes, l’entreprise <br/> <strong>M-C-T-P </strong>met toutes ses compétences et son savoir faire à votre service .<br/>
+  Des petits travaux de maçonnerie, réparations, rénovations complètes,<br/> constructions de maisons neuves, nous réalisons tous vos travaux de maçonnerie <br/> et de gros œuvre.</p>
+  <p>Vous avez un projet de construction, d'agrandissement ou de rénovation,<br/> nous sommes à votre écoute pour vous apporter la réponse la mieux adaptée <br/> et vous accompagner dans sa réalisation.
 
           </div>
           </section>
@@ -129,12 +130,7 @@
         <script src="js/index.js"></script>
         <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
         <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-<script src='http://demosthenes.info/assets/scripts/extras/shapes.js'></script>
-  
-        
 
-    
-    
-    
+      
   </body>
 </html>
