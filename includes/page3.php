aa3c98672cb8b48d2189b966c4ec4d3be646060b
OuterAlchemy <contact.thomasmerlin@gmail.com>
2016-07-28 15:34:49 +0200

    Finalisation des modales | Contenu à organiser dans les modales

--- includes/page3.php
+++ includes/page3.php
@@ -1,8 +1,8 @@
-<section class="l-accordion" id="section3">
+<section class="l-accordion " id="section3">
 	<div class="l-accordion__slide" data-accordion-slide="1">
 		<div class="l-accordion__inside">
 			<h2>
-                &nbsp;&nbsp;&nbsp;&nbsp;Maçonnerie
+                <button class='window1'>&nbsp;&nbsp;&nbsp;&nbsp;Maçonnerie</button>
             </h2>          
         </div>
     </div>
@@ -10,19 +10,45 @@
 	
 	<div class="l-accordion__slide" data-accordion-slide="2">
 		<div class="l-accordion__inside">
-			<h2>Carrelage</h2>
+            <h2>
+                <button class='window2'>Carrelage</button>
+            </h2>
 		</div>
 	</div>
 	
 	<div class="l-accordion__slide" data-accordion-slide="3">
 		<div class="l-accordion__inside">
-			<h2 id="categorie3">Terrassement</h2>
+			<h2>
+                <button class="window3">Terrassement</button>
+            </h2>
 		</div>
 	</div>
 	
 	<div class="l-accordion__slide" data-accordion-slide="4">
 		<div class="l-accordion__inside">
-			<h2>Placo</h2>
+			<h2>
+                <button class="window4">Placo</button>
+            </h2>
 		</div>
 	</div>
 </section>
+
+<div class="modal1">
+    <button class="close1">X</button>
+    Maçonnerie!<br />
+</div>
+
+<div class="modal2">
+    <button class="close2">X</button>
+    Carrelage !<br />
+</div>
+
+<div class="modal3">
+    <button class="close3">X</button>
+    Terrassement !<br />
+</div>
+
+<div class="modal4">
+    <button class="close4">X</button>
+    Placo !<br />
+</div>
--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" type="text/css" href="./assets/css/style.css" />
     <link rel="stylesheet" type="text/css" href="./assets/css/accordeonP3.css" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="js/prefixfree.min.js"></script>
+    <script src="./assets/js/prefixfree.min.js"></script>
 
     
   </head>
@@ -102,23 +102,8 @@
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
     <script src="./assets/js/loading.js" type="text/javascript"></script>
-    <!--<script>
-        $(document).on('ready', function(){
-            $('.loading1').addClass('loading-container');
-            $('.loading2').addClass('loading');
-            $('.loading-container').css('height', '100%');
-            $('#menu').css('display', 'none');  
-            
-            setTimeout(function(){
-                $('.loading1').fadeOut(800);
-            }, 2500);
-            
-            setTimeout(function() {
-                $('#menu').css('display', '');
-            }, 3500);
-            
-            
-        })
-        
-    </script>-->
+    <script src='./assets/js/modal.js' type="text/javascript"></script>
+    <script>
+       
+    </script>
 </html>
