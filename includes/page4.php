a8911994237e1bf3c5a0ec65fad8d07b90dde930
OuterAlchemy <contact.thomasmerlin@gmail.com>
2016-08-11 20:21:04 +0200

    modifications sections Activite et Realisation

--- includes/page4.php
+++ includes/page4.php
@@ -2,7 +2,7 @@
                                     <div class="l-accordion__slide" data-accordion-slide="1">
                                         <div class="l-accordion__inside">
                                             <h2>
-                                                <button class='windowRealisation1'>&nbsp;&nbsp;&nbsp;&nbsp;Maçonnerie</button>
+                                                <button class='windowRealisation1'>&nbsp;&nbsp;&nbsp;&nbsp;<span class="designInitiales">M</span>açonnerie</button>
                                             </h2>          
                                         </div>
                                     </div>
@@ -11,7 +11,7 @@
                                     <div class="l-accordion__slide" data-accordion-slide="2">
                                         <div class="l-accordion__inside">
                                             <h2>
-                                                <button class='windowRealisation2'>Carrelage</button>
+                                                <button class='windowRealisation2'><span class="designInitiales">C</span>arrelage</button>
                                             </h2>
                                         </div>
                                     </div>
@@ -19,7 +19,7 @@
                                     <div class="l-accordion__slide" data-accordion-slide="3">
                                         <div class="l-accordion__inside">
                                             <h2>
-                                                <button class="windowRealisation3">Terrassement</button>
+                                                <button class="windowRealisation3"><span class="designInitiales">T</span>errassement</button>
                                             </h2>
                                         </div>
                                     </div>
@@ -27,7 +27,7 @@
                                     <div class="l-accordion__slide" data-accordion-slide="4">
                                         <div class="l-accordion__inside">
                                             <h2>
-                                                <button class="windowRealisation4">Placo</button>
+                                                <button class="windowRealisation4"><span class="designInitiales">P</span>laco</button>
                                             </h2>
                                         </div>
                                     </div>
