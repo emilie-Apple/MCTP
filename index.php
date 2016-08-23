7d680608b1eb8ee6d8da32a13037eb32d090a8b7
OuterAlchemy <contact.thomasmerlin@gmail.com>
2016-08-23 13:16:29 +0200

    Réindentation & Creation fichier css responsive

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <head>
     <meta charset="UTF-8">
     <title>M.C.T.P</title>
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=yes" />
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=yes, width=device-width" />
     <meta name="description" content="MCTP site de maçonnerie">
     <meta name="keywords" content="maçonnerie, matériaux, maçon, béton, terrassement, carrelage, travaux publics, terrasse, MCTP">
     <meta property="og:image" content="https://pbs.twimg.com/media/CCNJN_XUMAAJSzU.jpg:large">
@@ -13,65 +13,82 @@
     <link rel="stylesheet" type="text/css" href="./assets/css/realisations.css" />
     <link rel="stylesheet" type="text/css" href="./assets/css/page4/carousel.css" />
     <link rel="stylesheet" type="text/css" href="./assets/css/style.css" />
+    <link rel="stylesheet" type="text/css" href="./assets/css/petitFormat.css" />
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   </head>
 
   <body> 
-
-    <div class="ct" id="t1">
-  <div class="ct" id="t2">
-    <div class="ct" id="t3">
-      <div class="ct" id="t4">
-         <div class="ct" id="t5">
-          <ul id="menu">
-
-            <a href="#t1"><li class="icon fa fa-home boutonMenu" id="uno"> Accueil</li></a>
-            <a href="#t2"><li class="icon fa fa-users boutonMenu" id="dos"> Qui sommes-nous ?</li></a>
-            <a href="#t3"><li class="icon fa fa-building boutonMenu" id="tres"> Activité</li></a>
-            <a href="#t4"><li class="icon fa fa-camera-retro boutonMenu" id="cuatro"> Réalisations</li></a>
-            <a href="#t5"><li class="icon fa fa-envelope boutonMenu" id="cinco"> Contact</li></a>
-            </ul>
-          <div class="page" id="p1">
-             <section class="accueil"></section> 
-          <div id="box1" class="box blurred-bg tinted">
-  <div class="content">
-      <h3><i class="fa fa-phone" aria-hidden="true">  06.34.18.31.90</i></h3>
-      
-  </div>
-</div>
-          </div>
-          <div class="page" id="p2">
-          <section class="icon fa fa-users"><span class="title">Qui sommes-nous ?</span>
-          <div id="circle-shape-example">
-            <img src="assets/img/logo.png" alt="logo" class="logo">
-            <h4>Présentation</h4>
-            <p>L'entreprise <strong>M-C-T-P </strong> situé dans la région du Loiret à <a data-toggle="tooltip" href="http://www.pagesjaunes.fr/pros/detail?bloc_id=0634183190558587290001C0001&no_sequence=0&code_rubrique=30450600#plan" title="Localisation">Nangeville (45330)</a> <br/>
-                est une entreprise de maçonnerie générale créée en 2013 par Fabien TECHER.</p>
-            <p>Avec l’aide de son associé Grégory et d’une équipe de 6 personnes, l’entreprise <br/>
-                <strong>M-C-T-P </strong>met toutes ses compétences et son savoir faire à votre service .<br/>
-                Des petits travaux de maçonnerie, réparations, rénovations complètes,<br/> 
-                constructions de maisons neuves, nous réalisons tous vos travaux de maçonnerie <br/> 
-                et de gros œuvre.</p>
-            <p>Vous avez un projet de construction, d'agrandissement ou de rénovation,<br/> 
-                nous sommes à votre écoute pour vous apporter la réponse la mieux adaptée <br/> 
-                et vous accompagner dans sa réalisation.
-
-          </div>
-          </section>
-          </div>  
-          <div class="page" id="p3">
-            <?php include('./includes/page3.php'); ?>
-          </div>
-          <div class="page" id="p4">
-            <?php include('./includes/page4.php'); ?>
-          </div> 
-          <div class="page" id="p5">
-            <?php include('./includes/page5.php'); ?>
+    <div class="grand-format">
+      <div class="ct" id="t1">
+        <div class="ct" id="t2">
+          <div class="ct" id="t3">
+            <div class="ct" id="t4">
+              <div class="ct" id="t5">
+                <ul id="menu">
+                  <a href="#t1">
+                    <li class="icon fa fa-home boutonMenu" id="uno"> Accueil</li>
+                  </a>
+                  <a href="#t2">
+                    <li class="icon fa fa-users boutonMenu" id="dos"> Qui sommes-nous ?</li>
+                  </a>
+                  <a href="#t3">
+                    <li class="icon fa fa-building boutonMenu" id="tres"> Activité</li>
+                  </a>
+                  <a href="#t4">
+                    <li class="icon fa fa-camera-retro boutonMenu" id="cuatro"> Réalisations</li>
+                  </a>
+                  <a href="#t5">
+                    <li class="icon fa fa-envelope boutonMenu" id="cinco"> Contact</li>
+                  </a>
+                </ul>
+                <div class="page" id="p1">
+                    <section class="accueil"></section> 
+                    <div id="box1" class="box blurred-bg tinted">
+                      <div class="content">
+                        <h3><i class="fa fa-phone" aria-hidden="true">  06.34.18.31.90</i></h3>
+                      </div>
+                    </div>
+                </div>
+                <div class="page" id="p2">
+                  <section class="icon fa fa-users"><span class="title">Qui sommes-nous ?</span>
+                    <div id="circle-shape-example">
+                      <img src="assets/img/logo.png" alt="logo" class="logo" />
+                      <h4>Présentation</h4>
+                        <p>L'entreprise <strong>M-C-T-P </strong> situé dans la région du Loiret à <a data-toggle="tooltip" href="http://www.pagesjaunes.fr/pros/detail?bloc_id=0634183190558587290001C0001&no_sequence=0&code_rubrique=30450600#plan" title="Localisation">Nangeville (45330)</a> <br/>
+                        est une entreprise de maçonnerie générale créée en 2013 par Fabien TECHER.</p>
+                        <p>Avec l’aide de son associé Grégory et d’une équipe de 6 personnes, l’entreprise <br/>
+                        <strong>M-C-T-P </strong>met toutes ses compétences et son savoir faire à votre service .<br/>
+                        Des petits travaux de maçonnerie, réparations, rénovations complètes,<br/> 
+                        constructions de maisons neuves, nous réalisons tous vos travaux de maçonnerie <br/> 
+                        et de gros œuvre.</p>
+                        <p>Vous avez un projet de construction, d'agrandissement ou de rénovation,<br/> 
+                        nous sommes à votre écoute pour vous apporter la réponse la mieux adaptée <br/> 
+                        et vous accompagner dans sa réalisation.
+                    </div>
+                  </section>
+                </div>  
+                <div class="page" id="p3">
+                  <?php include('./includes/page3.php'); ?>
+                </div>
+                <div class="page" id="p4">
+                  <?php include('./includes/page4.php'); ?>
+                </div> 
+                <div class="page" id="p5">
+                  <?php include('./includes/page5.php'); ?>
+                </div>
+                <footer>
+                <h1>Mentions légales  |  © 2016 Emilie Chartier & Thomas Merlin. All rights reserved.</h1>   
+                </footer>
+              </div>
             </div>
-<footer>
-<h1>Mentions légales  |  © 2016 Emilie Chartier & Thomas Merlin. All rights reserved.</h1>   
-</footer>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="petit-format">
+      <span>bonjour</span>
+    </div>
     
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <script type="text/javascript" src="./assets/js/bootstrap/bootstrap.min.js"></script>
